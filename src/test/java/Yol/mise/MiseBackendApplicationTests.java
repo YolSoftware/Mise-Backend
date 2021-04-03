1daa3797de4364afcbab7e27247980f35b9d7c35
tyrue <kdh9426@naver.com>
2021-04-04 00:36:39 +0900

    다시 처음부터

---
+++ src/test/java/Yol/mise/MiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Yol.mise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
