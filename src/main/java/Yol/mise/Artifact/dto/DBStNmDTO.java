9c07b8e74deea208f01376864425b6563dea8163
sungmu1 <soz7471@gmail.com>
2021-04-12 22:52:34 +0900

    database_0412

--- src/main/java/Yol/mise/Artifact/dto/DBStNmDTO.java
+++ src/main/java/Yol/mise/Artifact/dto/DBStNmDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+//측정소 이름을 저장하는 DTO
 @AllArgsConstructor // 자동으로 모든 매개변수를 받는 생성자를 생성
 @Getter // Getter 생성
 @Setter // Setter 생성
