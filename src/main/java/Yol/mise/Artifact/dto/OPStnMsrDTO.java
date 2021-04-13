5d72c32fe49b36e6df555eec61354570831d57cd
Just_NB <yanghs0540@naver.com>
2021-04-14 00:36:58 +0900

    Get realtime Station Measure data + DTO _0414

--- src/main/java/Yol/mise/Artifact/dto/OPStnMsrDTO.java
+++ src/main/java/Yol/mise/Artifact/dto/OPStnMsrDTO.java
@@ -1,41 +1,70 @@
 package Yol.mise.Artifact.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class OPStnMsrDTO {
-    private String date_time;
+    private String so2Grade;
+    private String so2Flag;
+    private String so2Value;
 
-    private String so2_flag;
-    private String so2_grade;
-    private String so2_value;
+    private String coGrade;
+    private String coFlag;
+    private String coValue;
 
-    private String co_flag;
-    private String co_grade;
-    private String co_value;
+    private String pm10Grade;
+    private String pm10Flag;
+    private String pm10Value;
 
-    private String pm10_flag;
-    private String pm10_grade;
-    private String pm10_value;
+    private String pm25Grade;
+    private String pm25Flag;
+    private String pm25Value;
 
-    private String khai_flag;
-    private String khai_value;
+    private String no2Grade;
+    private String no2Flag;
+    private String no2Value;
 
-    private String o3_flag;
-    private String o3_grade;
-    private String o3_value;
+    private String o3Grade;
+    private String o3Flag;
+    private String o3Value;
 
-    private String no2_flag;
-    private String no2_grade;
-    private String no2_value;
+    private String khaiValue;
+    private String khaiGrade;
 
+    private String dataTime;
 }
 
-        /*
-
-    {"so2Grade":"1","coFlag":null,"khaiValue":"73","so2Value":"0.004","coValue":"0.4",
-    "pm10Flag":null,"pm10Value":"36","o3Grade":"2","khaiGrade":"2","no2Flag":null,"no2Grade":"1","o3Flag":null,"so2Flag":null,"dataTime":"2021-04-09 17:00","coGrade":"1","no2Value":"0.014","pm10Grade":"2","o3Value":"0.058"}],"pageNo":1,"numOfRows":1},"header":{"resultMsg":"NORMAL_CODE","resultCode":"00"}}}
-         */
+//public class OPStnMsrDto {
+//    private String so2_grade;
+//    private String so2_flag;
+//    private String so2_value;
+//
+//    private String co_grade;
+//    private String co_flag;
+//    private String co_value;
+//
+//    private String pm10_grade;
+//    private String pm10_flag;
+//    private String pm10_value;
+//
+//    private String pm25_grade;
+//    private String pm25_flag;
+//    private String pm25_value;
+//
+//    private String no2_grade;
+//    private String no2_flag;
+//    private String no2_value;
+//
+//    private String o3_grade;
+//    private String o3_flag;
+//    private String o3_value;
+//
+//    private String khai_value;
+//    private String khai_grade;
+//
+//    private String date_time;
+//}
--- src/main/java/Yol/mise/Controller/AirApiController.java
+++ src/main/java/Yol/mise/Controller/AirApiController.java
@@ -1,59 +1,85 @@
 package Yol.mise.Controller;
 
 import Yol.mise.Artifact.dto.OPStnMsrDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Map;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/airapi")
 public class AirApiController {
-
-    @GetMapping("/stnmsr")
-    public String callStMsrApi() throws IOException {
-
-        String url_str = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/" +
-                "getMsrstnAcctoRltmMesureDnsty" +
-                "?stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC" +
-                "&dataTerm=month" +
-                "&pageNo=1" +
-                "&numOfRows=1" +
-                "&returnType=json" +
-                "&serviceKey=vzt9ZGiRSLznVI%2FpSr%2BL4Lth9rr8jHKqp4RRyDX0cvUfeiA4w6Chfr1hyGaX%2FGsXuxNH22VKBgxq8vY6svNLNw%3D%3D";
-        URL url = new URL(url_str);
+    final String base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/";
+    final String service_key = "X87euFz3fd072hiDInhC%2F%2BvESJAmhyTBt%2FfIQT0iLvZiC3UZEDAcVtSZxNUZqW9GVaaRi%2BaCeL1Oz7ss8Scklw%3D%3D";
+    public String httpConnection(URL url) throws IOException {
         HttpURLConnection url_connection = (HttpURLConnection) url.openConnection();
         url_connection.setRequestMethod("GET");
-        OPStnMsrDTO stn_msr = new OPStnMsrDTO();
-
-
-        if (url_connection.getResponseCode() == 200) {
-            // JACSON 라이브러리를 이용한 JSON PARSING
-            ObjectMapper mapper = new ObjectMapper();
-
-            Map<String, Object> raw_json = mapper.readValue(url_connection.getInputStream(), Map.class);
-            System.out.println(raw_json);
-
-            Map<String, Object> tmp = (Map<String, Object>) raw_json.get("response");
-            tmp = (Map<String, Object>) tmp.get("body");
-
-            //stn_msr = mapper.readValue(items, OPStnMsrDTO.class);
-            System.out.println(tmp);
-
+        String raw_string = "";
+        if(url_connection.getResponseCode() == 200) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(url_connection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            raw_string = sb.toString();
         }
-        /*
-"items":[{"so2Grade":"1","coFlag":null,"khaiValue":"73","so2Value":"0.004","coValue":"0.4","pm10Flag":null,"pm10Value":"36",
-"o3Grade":"2","khaiGrade":"2","no2Flag":null,"no2Grade":"1","o3Flag":null,"so2Flag":null,"dataTime":"2021-04-09 17:00",
-"coGrade":"1","no2Value":"0.014","pm10Grade":"2","o3Value":"0.058"}],"pageNo":1,"numOfRows":1}}
-         */
-        return "0";
+        return raw_string;
     }
+
+    @GetMapping("/stnmsr/{stn}")
+    public Object callStMsrApi(@PathVariable String stn) throws IOException{
+        JsonObject raw_json;
+        Gson gson = new Gson();
+        int data_count = 0;
+        String encode_stn = URLEncoder.encode(stn, "UTF-8");
+        String api_url = base_url + "getMsrstnAcctoRltmMesureDnsty";
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+        UriComponents uri_components = UriComponentsBuilder.fromHttpUrl(api_url)
+                .queryParam("stationName", encode_stn)
+                .queryParam("dataTerm","daily")
+                .queryParam("pageNo","1")
+                .queryParam("numOfRows","1")
+                .queryParam("returnType","json")
+                .queryParam("serviceKey",service_key)
+                .queryParam("ver","1.0")
+                .build();
+
+        URL url = new URL(uri_components.toString());
+        String raw_string = httpConnection(url);
+
+        raw_json = gson.fromJson(raw_string, JsonObject.class);
+        raw_json = gson.fromJson(raw_json.get("response"), JsonObject.class);
+        raw_json = gson.fromJson(raw_json.get("body"), JsonObject.class);
+        data_count = Integer.parseInt(raw_json.get("totalCount").toString());
+
+        if (data_count > 0) {
+            Type listType = new TypeToken<ArrayList<OPStnMsrDTO>>(){}.getType();
+            List<OPStnMsrDTO> stn_msr_dto = gson.fromJson(raw_json.get("items").toString(), listType);;
+            return stn_msr_dto;
+        } else {
+            return "잘못된 스테이션 이름 입력";
+        }
+    }
+
 
 }
