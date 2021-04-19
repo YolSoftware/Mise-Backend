4ec2958628e05ed5bb010ad5c7ac2532d0db5b6e
tyrue <kdh9426@naver.com>
2021-04-19 21:57:47 +0900

    add : 디비에서 데이터 가져와서 보내주는 코드 더미 생성

--- src/main/java/Yol/mise/Controller/GetGPSController.java
+++ src/main/java/Yol/mise/Controller/GetGPSController.java
@@ -1,20 +1,28 @@
 package Yol.mise.Controller;
 
+import Yol.mise.Artifact.dao.DBStNmDAO;
 import Yol.mise.Artifact.dto.DBStNmDTO;
 import com.google.gson.JsonObject;
 import lombok.Data;
+import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping({"/api", "/api/misenow"})
 public class GetGPSController {
     int count = 0;
 
-    
+    DBStNmDAO dbStNmDAO;
+    @PostMapping ("/location")
+    public String getPostLocation (@RequestBody @Validated GetLocation location) {
+        System.out.println("위도 : " + location.latitude + " 경도 : " + location.longitude);
+        return "위도 : " + location.latitude + " 경도 : " + location.longitude;
+    }
 
     @GetMapping("/location")
     public String getLocation (@RequestBody @Validated GetLocation location) {
@@ -38,6 +46,21 @@ public class GetGPSController {
     @ResponseBody
     @GetMapping("/fineDust")
     public String dustData () {
+        DBStNmDTO dbStNmDTO = new DBStNmDTO("2", "2", "2", "2", "2");
+        List<DBStNmDTO> testList;
+        try{
+            System.out.println("1");
+            testList = dbStNmDAO.selectUsers(dbStNmDTO);
+            System.out.println("2");
+            if(testList.size() > 0) {
+                for (DBStNmDTO t : testList) {
+                    System.out.println(t.getStation_x());
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+
         count++;
 
         JsonObject nowData = new JsonObject();
